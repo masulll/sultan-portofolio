72a8e6a1e4df5edd8ac73b4c0d166fa0b21e3d78
Sultan R <sultanr2409@gmail.com>
2024-02-07 16:55:21 +0700

    fix download button

--- src/components/NameField/Downloadbutton.jsx
+++ src/components/NameField/Downloadbutton.jsx
@@ -5,7 +5,7 @@ const Downloadbutton = () => {
   return (
     <>
       <Link
-        href="https://igrskdzqqnxqrnrgzktm.supabase.co/storage/v1/object/public/bukti_tf/Sultan%20Rayhan,%20S.Kom-%20Latest%20CV.pdf"
+        href="https://drive.google.com/file/d/1HBdOm70nFwOFGkv2mRtcBIyK87xUNlkQ/view?usp=drive_link"
         target="blank"
         download
         className="bg-sky-600  px-8 rounded-md  flex items-center hover:bg-sky-400 transition-all"
